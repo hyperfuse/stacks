07678d377dffc9a3bba7b10c35da6343b3daad95
Guillaume Breton <breton.gy@gmail.com>
2025-04-11 21:52:20 +0200

    Initial commit

---
+++ lib/stacks/mailer.ex
@@ -0,0 +1,3 @@
+defmodule Stacks.Mailer do
+  use Swoosh.Mailer, otp_app: :stacks
+end
---
+++ test/stacks_web/controllers/error_json_test.exs
@@ -0,0 +1,12 @@
+defmodule StacksWeb.ErrorJSONTest do
+  use StacksWeb.ConnCase, async: true
+
+  test "renders 404" do
+    assert StacksWeb.ErrorJSON.render("404.json", %{}) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500" do
+    assert StacksWeb.ErrorJSON.render("500.json", %{}) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
