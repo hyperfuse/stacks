349cab3f071e01d4294acac6e55071897a3ffda1
Guillaume Breton <breton.gy@gmail.com>
2025-06-06 21:41:40 +0200

    Use nanoid instead of sequences

--- lib/stacks/items/item.ex
+++ lib/stacks/items/item.ex
@@ -2,15 +2,21 @@ defmodule Stacks.Items.Item do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :string, autogenerate: false}
+  @foreign_key_type :string
+
   schema "items" do
     field :item_type, :string
     field :metadata, :map
     field :source_url, :string
     field :text_content, :string
     field :enrichment_status, Ecto.Enum, values: [:pending, :processing, :completed, :failed], default: :pending
-    field :user_id, :id
+    field :user_id, :string
     timestamps(type: :utc_datetime)
   end
+
+  @doc false
+  def generate_id, do: Nanoid.generate()
 
   @doc false
   def changeset(item, attrs) do
@@ -18,5 +24,11 @@ defmodule Stacks.Items.Item do
     |> cast(attrs, [:item_type, :source_url, :text_content, :metadata, :enrichment_status])
     |> validate_required([:item_type, :source_url])
     |> validate_inclusion(:enrichment_status, [:pending, :processing, :completed, :failed])
+    |> put_id()
   end
+
+  defp put_id(%Ecto.Changeset{data: %{id: nil}} = changeset) do
+    put_change(changeset, :id, generate_id())
+  end
+  defp put_id(changeset), do: changeset
 end
--- lib/stacks/stacks/item.ex
+++ lib/stacks/stacks/item.ex
@@ -2,21 +2,33 @@ defmodule Stacks.Stacks.Item do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :string, autogenerate: false}
+  @foreign_key_type :string
+
   schema "items" do
     field :item_type, :string
     field :metadata, :map
     field :source_url, :string
     field :text_content, :string
     field :title, :string
-    field :user_id, :id
+    field :user_id, :string
 
     timestamps(type: :utc_datetime)
   end
+
+  @doc false
+  def generate_id, do: Nanoid.generate()
 
   @doc false
   def changeset(item, attrs) do
     item
     |> cast(attrs, [:item_type, :source_url, :title, :text_content, :metadata])
     |> validate_required([:item_type, :source_url, :title, :text_content])
+    |> put_id()
   end
+
+  defp put_id(%Ecto.Changeset{data: %{id: nil}} = changeset) do
+    put_change(changeset, :id, generate_id())
+  end
+  defp put_id(changeset), do: changeset
 end
