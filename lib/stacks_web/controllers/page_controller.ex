4743754e9383762e029c794726c03f10dae73606
Guillaume Breton <guillaumebreton@users.noreply.github.com>
2025-06-20 16:46:30 +0200

    Guillaumebreton/improve design (#16)
    * Start the redesign
    
    * Remove the archive link
    
    * Improve the top description part
    
    * Fix the top bar
    
    * Make the side bar collapse
    
    * Improve color scheme
    
    * Add summary
    
    * Improve the size
    
    * Remove the collections section
    
    * Improve layout
    
    * Remove the search field
    
    * Create a dedicated video and articles page
    
    * Fix icons
    
    * Add the icons everywhere

--- lib/stacks_web/controllers/page_controller.ex
+++ lib/stacks_web/controllers/page_controller.ex
@@ -8,6 +8,18 @@ defmodule StacksWeb.PageController do
     render(conn, :home, items: items)
   end
 
+  def articles(conn, _params) do
+    articles = Items.list_items_with_associations()
+    |> Enum.filter(&(&1.item_type == "article" && &1.article))
+    render(conn, :articles, items: articles)
+  end
+
+  def videos(conn, _params) do
+    videos = Items.list_items_with_associations()
+    |> Enum.filter(&(&1.item_type == "video" && &1.video))
+    render(conn, :videos, items: videos)
+  end
+
   def item(conn, %{"id" => id}) do
     item = Items.get_item_with_associations!(id)
     render(conn, :item, item: item)
